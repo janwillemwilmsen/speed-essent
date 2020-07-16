e3912a18062d1fa9195916f63500cc740b2eb1f6
Jan Willem Wilmsen <janwillemwilmsen@gmail.com>
2020-07-16 18:12:57 +0200

    fix irls

---
+++ _data/sites/02energiedirect.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: "energiedirect.nl", // optional, falls back to object key
+	description: "energiedirect web sites",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+		"https://www.energiedirect.nl/isolatie",
+		"https://www.energiedirect.nl/",
+		"https://www.energiedirect.nl/zonnepanelen",
+		"https://www.energiedirect.nl/energie/energie-aanbiedingen",
+		"https://www.energiedirect.nl/energie/goedkope-energie",
+		// "https://www.energiedirect.nl/energie/overstappen-energie",
+		// "https://www.energiedirect.nl/energie/energie-besparen",
+		// "https://www.energiedirect.nl/energie/goedkope-energie",
+		// "https://www.energiedirect.nl/energie/energiecontract",
+	]
+};
---
+++ _data/sites/03servicepartners.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: "Servicepartners", // optional, falls back to object key
+	description: "Servicepartners web sites",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		freshChrome: "run", // verschillende sites is RUN
+	},
+	urls: [
+		"https://www.kemkens.nl/",
+		"https://www.voltasolar.nl/",
+		"https://www.ewacht.nl/",
+		// "https://www.energiewacht.nl/",
+		"https://www.geas.nl/",
+		// "https://www.voltalimburg.nl/",
+		"https://www.energiewonen.nl/",
+		"https://www.zon7.nl/",
+		"https://www.isoprofs.nl/",
+		"https://www.nederlandisoleert.nl/",
+		"https://www.cvtotaal.nl/",
+		// "https://www.klimaatroute.nl/",
+	]
+};
