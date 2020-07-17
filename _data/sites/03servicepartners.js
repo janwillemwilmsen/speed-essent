b85f89d511ad3b4e7843f6bb21f2c1a75fc4a659
Jan Willem Wilmsen <janwillemwilmsen@gmail.com>
2020-07-18 01:47:27 +0200

    freq

--- _data/sites/03servicepartners.js
+++ _data/sites/03servicepartners.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Servicepartners", // optional, falls back to object key
 	description: "Servicepartners websites",
 	options: {
-		frequency: 60 * 23, // 24 hours
+		frequency: 1 * 1, // 24 hours
 		freshChrome: "run", // verschillende sites is RUN
 	},
 	urls: [
