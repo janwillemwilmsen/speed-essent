9ed42318b7eec7135cb8dfedc5197019ea515d24
Jan Willem Wilmsen <janwillemwilmsen@gmail.com>
2020-07-19 22:01:03 +0200

    enbranch1

--- _data/sites/03servicepartners.js
+++ _data/sites/03servicepartners.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Servicepartners", // optional, falls back to object key
 	description: "Servicepartners websites",
 	options: {
-		frequency: 1 * 1, // 24 hours 60*23
+		frequency: 60 * 23, // 24 hours 60*23
 		freshChrome: "run", // verschillende sites is RUN
 	},
 	urls: [
