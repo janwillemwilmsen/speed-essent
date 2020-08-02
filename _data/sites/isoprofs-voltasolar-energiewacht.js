99223393e2534b2d2789de1320242176ad825873
Jan Willem Wilmsen <janwillemwilmsen@gmail.com>
2020-08-02 15:57:27 +0200

    separate sps

---
+++ _data/sites/isoprofs-voltasolar-energiewacht.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: "Servicepartners", // optional, falls back to object key
+	description: "Servicepartners websites",
+	options: {
+		frequency: 1 * 1, 
+		// frequency: 60 * 23, 
+		// 24 hours
+		freshChrome: "run", // verschillende sites is RUN
+	},
+	urls: [
+		 
+		"https://isoprofs.nl/",
+		 
+		"https://www.voltasolar.nl/",
+		"https://www.energiewacht.nl/"
+	]
+};
